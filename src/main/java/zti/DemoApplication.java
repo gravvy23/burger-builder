045da27cc0346078117d5a89879409643c4b7391
gravvy23 <majkutw@gmail.com>
2020-05-23 15:24:35 +0200

    add basics

--- src/main/java/zti/DemoApplication.java
+++ src/main/java/zti/DemoApplication.java
@@ -1,21 +1,34 @@
-package com.example.demo;
+package zti;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
-@Controller
-@SpringBootApplication
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+
+@Component
 public class DemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DemoApplication.class.getPackage().getName());
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @PostConstruct
+    public void myRealMainMethod() throws SQLException {
+        Statement stmt = dataSource.getConnection().createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ingredients");
+        stmt.executeUpdate("CREATE TABLE ingredients ( id serial primary key, name varchar, price decimal(5,2))");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('salad', 0.50)");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('cheese', 0.40)");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('meat', 1.30)");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('bacon', 0.70)");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('tomato', 0.50)");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('cucumber', 0.40)");
+        stmt.executeUpdate("INSERT INTO ingredients (name, price) VALUES ('onion', 0.40)");
     }
 }
